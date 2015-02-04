abe2834a5d06aa8bdde796eccc1ee78cf2bbe0f4
Gabi Nagy <gabrian.nagy@gmail.com>
2015-02-04 20:26:08 +0200

    Added sneakyness-ness-ness

--- src/js/01-script.js
+++ src/js/01-script.js
@@ -2,9 +2,43 @@
  * Section
  * ======================================================================= */
 
-$(function() {
+function nightMode() {
+  var link = document.createElement('link');
+  link.id = 'night-mode';
+  link.rel = 'stylesheet';
+  link.href = '/assets/night-mode.css';
+  document.head.appendChild(link);
+  document.querySelector('link[rel=icon]').href = '/assets/icon-night.ico';
+}
 
-});
+function dayMode() {
+  var link = document.getElementById('night-mode');
+  link.parentNode.removeChild(link);
+  document.querySelector('link[rel=icon]').href = '/assets/icon.ico';
+}
+
+document.getElementById('site-header').onclick = function(event) {
+  if (event.target.id != 'site-header') {
+    return;
+  }
+  if (document.getElementById('night-mode') == null) {
+    nightMode();
+  } else {
+    dayMode();
+  }
+}
+
+var date = new Date();
+if (date.getHours() >= 21 || date.getHours() <= 6) {
+  nightMode();
+}
+
+console.error('GET http://a.life.com/');
+console.log('');
+console.log("Just kidding. I love snooping into people's websites as well :)");
+console.log("Here's a cat for your effort: http://youtu.be/A67OAOyJ9Dk");
+console.log('');
+console.log('Have a good one!');
 
 /* =======================================================================
  * Section
